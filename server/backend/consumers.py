e7a50510a5a8239bb2ad963e5bbb9a98de2dde99
Animesh Risal <animeshrisal99@gmail.com>
2021-01-18 21:24:14 +0545

    Add basic consumer

--- server/backend/consumers.py
+++ server/backend/consumers.py
@@ -1,6 +1,13 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
- 
-class NotificationConsumer(AsyncWebsocketConsumer):
-    async def websocket_connect(self, event):
-        user = self.scope["user"]
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+class NotificationConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
         await self.accept()
+
+    async def receive_json(self, text_data=None, bytes_data=None):
+        await self.send_json({
+            'test': 1
+        })
+
+    async def disconnect(self, close_code):
+        await self.close()
