96879f65bc1fe92884b75d107d7aa22747a57616
Animesh Risal <animeshrisal99@gmail.com>
2021-05-04 15:05:19 +0545

    Lint app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import NavBar, { } from "./shared/NavBar"
+import NavBar from "./shared/NavBar";
 
 import Login from "./pages/Login";
 import Project from "./pages/Project";
 import { PrivateRoute } from "./shared/PrivateRoute";
 import Profile from "./pages/Profile";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 import Sprint from "./pages/Sprint";
 import { AuthenticationProvider } from "./context/AuthContext";
 import Notification from "./pages/Notification";
@@ -16,24 +16,29 @@ import Board from "./pages/Board";
 const queryClient = new QueryClient();
 
 function App() {
-
-
   return (
     <AuthenticationProvider>
-    <QueryClientProvider client={queryClient}>
-      <Router>
+      <QueryClientProvider client={queryClient}>
+        <Router>
           <NavBar />
           <div>
             <Route exact path="/login" component={Login} />
-            <PrivateRoute exact path="/projects" component={Project} /> 
+            <PrivateRoute exact path="/projects" component={Project} />
             <PrivateRoute exact path="/profile" component={Profile} />
             <PrivateRoute exact path="/notification" component={Notification} />
-            <PrivateRoute exact path="/projects/:projectId" component={Sprint} />
-            <PrivateRoute exact path="/projects/:projectId/sprint/:sprintId/board" component={Board} />
+            <PrivateRoute
+              exact
+              path="/projects/:projectId"
+              component={Sprint}
+            />
+            <PrivateRoute
+              exact
+              path="/projects/:projectId/sprint/:sprintId/board"
+              component={Board}
+            />
           </div>
-
-      </Router>
-    </QueryClientProvider>
+        </Router>
+      </QueryClientProvider>
     </AuthenticationProvider>
   );
 }
