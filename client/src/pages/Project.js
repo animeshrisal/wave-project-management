aff79c4c8a787da820fc3fc1beebb2b0a648ec17
Animesh Risal <animeshrisal99@gmail.com>
2021-05-04 13:24:24 +0545

    Fix project list

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,6 +1,8 @@
+import { Field, Form, Formik } from "formik";
 import React, { useState } from "react";
 
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
+import { useMutation } from "react-query";
 import Modal from "../components/Modal";
 import ProjectCard from "../components/ProjectCard";
 import projectService from "../network/projectService";
@@ -8,6 +10,9 @@ import projectService from "../network/projectService";
 import "./Project.scss"
 
 export default function Project(props) {
+
+
+  const queryClient = useQueryClient();
   const { isLoading, data, error } = useQuery(
     "projects",
     projectService.getProjectList
@@ -27,6 +32,12 @@ export default function Project(props) {
     setIsModalVisible(false);
   };
 
+  const mutation = useMutation((project) => projectService.createProject(project), {
+    onSuccess: () => {
+      queryClient.invalidateQueries("projects")
+    }
+  });
+
 
   if (isLoading) return "Loading...";
 
@@ -43,6 +54,22 @@ export default function Project(props) {
             onCancel={handleCancel}
             >
               Add Project
+              <Formik
+                initialValues={{name: ''}}
+                onSubmit={async (values) => {
+                  mutation.mutate(values)
+                }}
+              >
+                {({isSubmitting}) => (
+                <Form>
+                  <label htmlFor="name">Project Name</label>
+                  <Field id="name" name="name" placeholder="Example" />
+                  <button type="submit" disabled={isSubmitting} >
+                    Submit
+                  </button>
+                </Form>
+                )}
+              </Formik>
           </Modal>
           <button onClick={showModal} />
       <div className="grid-container">
